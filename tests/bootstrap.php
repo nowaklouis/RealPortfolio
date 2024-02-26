7c3caee6ea7539e06c0c8bd194ccde1b17b076fe
louis nowak <nowak.louis@gmail.com>
2024-02-26 15:43:10 +0100

    config dotenv

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,6 +6,6 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__) . '/config/bootstrap.php')) {
     require dirname(__DIR__) . '/config/bootstrap.php';
-} elseif ($_SERVER['APP_ENV'] !== 'prod' && method_exists(Dotenv::class, 'bootEnv')) {
+} elseif ((!isset($_ENV['APP_ENV']) || $_ENV['APP_ENV'] !== 'prod') && method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
