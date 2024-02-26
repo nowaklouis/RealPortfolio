e63989efa9677d4f4543ad61e28fff90262c541a
louis nowak <nowak.louis@gmail.com>
2024-02-26 16:10:55 +0100

    config dotenv

--- public/index.php
+++ public/index.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Kernel;
+use Symfony\Component\Dotenv\Dotenv;
 
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
+require_once dirname(__DIR__) . '/vendor/autoload_runtime.php';
+
+if ($_SERVER['APP_ENV'] !== 'prod') {
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
