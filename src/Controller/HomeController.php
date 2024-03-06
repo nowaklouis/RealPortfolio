1a09b58a3f01298be7ad5f404d833b6586bce041
louis nowak <nowak.louis@gmail.com>
2024-03-06 10:41:19 +0100

    suppression contact

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,42 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Mailer\MailerInterface;
-use App\Form\ContactFormType;
 
 class HomeController extends AbstractController
 {
-    public function __construct(
-        private readonly MailerInterface $mailer
-    ) {
+    public function __construct()
+    {
     }
 
     #[Route('/', name: 'index')]
-    public function index(Request $request): Response
+    public function index(): Response
     {
-        $formData = [];
-        $form = $this->createForm(ContactFormType::class, $formData);
 
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $formData = $form->getData();
-
-            $message = (new \Symfony\Component\Mime\Email())
-                ->from($formData['email'])
-                ->to('nowak.louis@gmail.com')
-                ->subject($formData['sujet'])
-                ->text($formData['email']);
-            $this->mailer->send($message);
-
-            $this->addFlash('success', 'Votre message a été envoyé avec succès.');
-
-            return $this->redirectToRoute('index');
-        }
-
-        return $this->render('home/index.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render('home/index.html.twig');
     }
 }
